ef766efc6e663bf906769bd32191e545222f2116
Santiago072 <santiagolizcanosuarez@gmail.com>
2026-08-06 17:17:36 -0500

    fix: asegurar que las variables de config/.env prevalezcan sobre las del entorno del contenedor

--- config/EnvLoader.php
+++ config/EnvLoader.php
@@ -35,11 +35,10 @@ class EnvLoader
             $key   = trim($key);
             $value = trim($value);
 
-            // No sobreescribir variables de entorno reales del sistema operativo
-            if (!array_key_exists($key, $_ENV) && getenv($key) === false) {
-                $_ENV[$key] = $value;
-                putenv("$key=$value");
-            }
+            // Asignar la variable de entorno y actualizar putenv()
+            $_ENV[$key] = $value;
+            putenv("$key=$value");
+
         }
     }
 }
