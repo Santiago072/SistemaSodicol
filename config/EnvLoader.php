780778bb1edc4c1fe53a3b4e28d5b7c0f16a8b32
Santiago072 <santiagolizcanosuarez@gmail.com>
2026-08-18 16:37:09 -0500

    Revert: Restaurar config de conexion y EnvLoader al estado funcional original del VPS

--- config/EnvLoader.php
+++ config/EnvLoader.php
@@ -41,11 +41,9 @@ class EnvLoader
             $key   = trim($key);
             $value = trim($value);
 
-            // No sobreescribir variables de entorno reales del contenedor/sistema operativo
-            if (!array_key_exists($key, $_ENV) && getenv($key) === false) {
-                $_ENV[$key] = $value;
-                putenv("$key=$value");
-            }
+            // Asignar la variable de entorno y actualizar putenv()
+            $_ENV[$key] = $value;
+            putenv("$key=$value");
 
         }
     }
